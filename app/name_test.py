5c8137af0cb6c38bfc132b16e090829a6658cd3b
Andrew Ma <andrewma83@hotmail.com>
2018-11-24 11:40:07 +0800

    Update and fixed the jinga template loop, so that Python side can pass loop list to the template for display.

---
+++ app/name_test.py
@@ -0,0 +1,5 @@
+class name:
+    def __init__(self, f_name, l_name, u_name):
+        self.f_name = f_name
+        self.l_name = l_name
+        self.u_name = u_name
--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,7 @@ from flask import render_template
 
 from app import app
 from app import form_util
+from app import name_test
 
 
 def generate_file_list():
@@ -18,11 +19,15 @@ def generate_file_list():
 
     return filelist
 
-
 @app.route('/')
 def home():
     try:
-        return render_template('index.html')
+        param_list = [name_test.name("Hello", "Kitty", "@Saniro")]
+        param_list.append(name_test.name("Mark", "Otto", "@motto"))
+        param_list.append(name_test.name("Jacob", "Thortton", "@fat"))
+        param_list.append(name_test.name("Larry", "Bird", "@Twitter"))
+
+        return render_template('index.html', table_params=param_list)
     except Exception as e:
         return str(e)
 
