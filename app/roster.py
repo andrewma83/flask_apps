3c6ae5b00c14977726687117846864870758dafa
Andrew Ma <andrewma83@hotmail.com>
2018-12-29 23:26:41 -0800

    Clean up template and remove loop index row from the table. Create dynamic change on year and church database.

---
+++ app/roster.py
@@ -0,0 +1,8 @@
+class info:
+    def __init__(self, date, event, speaker_chi, speaker_eng, presider, slide_ctrl):
+        self.date = date
+        self.event = event
+        self.speaker_chi = speaker_chi
+        self.speaker_eng = speaker_eng
+        self.presider = presider
+        self.slide_ctrl = slide_ctrl
--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+import sqlite3
 from os import listdir
 from os.path import isfile, join
 
@@ -5,7 +6,7 @@ from flask import render_template
 
 from app import app
 from app import form_util
-from app import name_test
+from app import roster
 
 
 def generate_file_list():
@@ -20,17 +21,52 @@ def generate_file_list():
     return filelist
 
 
-@app.route('/')
-def home():
-    try:
-        param_list = [name_test.name("Hello", "Kitty", "@Saniro"),
-                      name_test.name("Mark", "Otto", "@motto"),
-                      name_test.name("Jacob", "Thortton", "@fat"),
-                      name_test.name("Larry", "Bird", "@Twitter")]
+def retrieve_church_roster_info(dbFile_name):
+    sql_stmt = "SELECT date, event, speaker_chi, speaker_eng, presider, slide_ctrl from roster"
+    result_set = []
+    print("Attempt open sqlite3 database file: %s" % dbFile_name)
+    conn = sqlite3.connect(dbFile_name)
+    with conn:
+        try:
+            cur = conn.cursor()
+            rows = cur.execute(sql_stmt)
 
-        return render_template('index.html', table_params=param_list)
+            for row in rows:
+                result_set.append(row)
+
+        except sqlite3.Error as e:
+            print("Database error: %s" % e)
+        except Exception as e:
+            print("Exception in _query: %s" % e)
+
+    if conn:
+        print("Close sqlite3 database file: %s" % dbFile_name)
+        conn.close()
+
+    return result_set
+
+
+def render_roster_info(year):
+    try:
+        rows = retrieve_church_roster_info("/mnt/church_%s.db" % year)
+        # rows = retrieve_church_roster_info("church.db")
+        param_list = []
+        for row in rows:
+            param_list.append(roster.info(row[0], row[1], row[2], row[3], row[4], row[5]))
+
+        return render_template('roster_template.html', year=int(year), table_params=param_list)
     except Exception as e:
         return str(e)
+
+
+@app.route('/2018')
+def render_2018_roster():
+    return render_roster_info("2018")
+
+
+@app.route('/2019')
+def render_2019_roster():
+    return render_roster_info("2019")
 
 
 @app.route('/filelist')
